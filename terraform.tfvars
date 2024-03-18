8b0f78def00b9b38ee0a2d6e6bd7970140fc6ba2
kshartul <kumarshartul24@gmail.com>
2024-03-19 01:36:48 +0530

    The AKS deployment

---
+++ terraform.tfvars
@@ -0,0 +1,9 @@
+resource_group_name         = "rg-stg-aks"
+region                      = "eastus"
+vnetCIDR                    = ["10.163.0.0/16"]
+subnetCIDRs                 = ["10.163.0.0/21"]
+environment                 = "staging"
+max_node_count              = 4
+min_node_count              = 2
+node_count                  = 2
+node_vm_size                = "Standard_D2_v2"
---
+++ variables.tf
@@ -0,0 +1,56 @@
+# resource group name
+variable "resource_group_name" {
+  type        = string
+  description = "Resource Group name"
+}
+
+# region
+variable "region" {
+  type        = string
+  description = "Region"
+}
+
+# Virtual Network CIDR
+variable "vnetCIDR" {
+  type        = list(string)
+  description = "Virtual Network CIDR"
+}
+
+# subnet CIDRs
+variable "subnetCIDRs" {
+  type        = list(string)
+  description = "Subnet CIDRs"
+}
+
+# environment
+variable "environment" {
+  type        = string
+  description = "Environment"
+}
+
+# max node count 
+variable "max_node_count" {
+  type        = number
+  description = "Maximun node count for worker node"
+}
+
+# min node count 
+variable "min_node_count" {
+  type        = number
+  description = "Minimum node count for worker node"
+}
+
+# size of worker node
+variable "node_vm_size" {
+  type        = string
+  description = "Size of worker node"
+}
+# node count
+variable "node_count" {
+  type        = number
+  description = "The required node count for worker node"
+}
+variable "admin_username" {
+  description = "Admin username for cluster nodes."
+  default     = "azureuser"
+}
