78a32f0b882b7eebc0a816c0834fcfbedb518167
kshartul <kumarshartul24@gmail.com>
2024-03-19 13:34:01 +0530

    The backend deployment

--- provider.tf
+++ provider.tf
@@ -13,7 +13,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = var.state_resource_group_name
     storage_account_name = var.state_storage_account_name
-    container_name       = "aks"
+    container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
 }
