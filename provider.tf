2149c11b668a665ee134d7a8140dc400b81fb797
kshartul <kumarshartul24@gmail.com>
2024-03-20 17:27:49 +0530

    aks deployment

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.94.0"
     }
-    required_version = "1.1.0"
+    required_version = ">= 1.1.0"
   }
   backend "azurerm" {
     #resource_group_name  = "aksstracc01" #var.state_resource_group_name
